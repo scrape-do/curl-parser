41efa2bf03d45765a567e95f21adf8a7330017ff
TKSun <1037305420@qq.com>
2026-04-29 09:15:22 +0000

    test: add case for handling --data-raw with stringified JSON

--- tests/data-raw.spec.ts
+++ tests/data-raw.spec.ts
@@ -21,4 +21,13 @@ describe('data-raw', () => {
     expect(cmd.body).toBe('key=value&foo=bar');
     expect(cmd.bodyArg).toBe('raw');
   });
+
+  it('should handle --data-raw with stringified json', () => {
+    const raw = JSON.stringify({ foo: "bar", key: JSON.stringify([{ foo: 'bar' }]) })
+    console.log(`curl --data-raw '${raw}' http://example.com`)
+    const cmd = parse(`curl --data-raw "${raw}" http://example.com`);
+
+    expect(cmd.body).toBe(raw);
+    expect(cmd.bodyArg).toBe('raw');
+  });
 });
