920fe50317c173f38c7d676b286a87cb7d385241
TKSun <1037305420@qq.com>
2026-04-29 09:20:20 +0000

    chore: remove debug log from data-raw test

--- tests/data-raw.spec.ts
+++ tests/data-raw.spec.ts
@@ -24,7 +24,6 @@ describe('data-raw', () => {
 
   it('should handle --data-raw with stringified json', () => {
     const raw = JSON.stringify({ foo: "bar", key: JSON.stringify([{ foo: 'bar' }]) })
-    console.log(`curl --data-raw '${raw}' http://example.com`)
     const cmd = parse(`curl --data-raw "${raw}" http://example.com`);
 
     expect(cmd.body).toBe(raw);
